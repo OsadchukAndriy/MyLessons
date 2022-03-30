82086dc7563465eb329c55eebc818b836b6738b2
andrejosadcuk <Rudyst.1995@gmail.com>
2022-03-30 19:18:28 +0300

    udemy course update ex8 ex9

--- src/Multithreading/Ex7.java
+++ src/Multithreading/Ex7.java
@@ -2,10 +2,12 @@ package Multithreading;
 
 public class Ex7 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         for (int i = 5; i > 0; i-- ){
             System.out.println(i);
+            Thread.sleep(1000);
         }
+        System.out.println("Поєхали!!");
 
     }
 }
