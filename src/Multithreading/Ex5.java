e39043c8d81fa403bbd2527c0ebc3f6cb0b7f6cc
andrejosadcuk <Rudyst.1995@gmail.com>
2022-03-30 18:27:36 +0300

    udemy course update

---
+++ src/Multithreading/Ex5.java
@@ -0,0 +1,20 @@
+package Multithreading;
+
+public class Ex5 {
+    public static void main(String[] args) {
+
+        MyThread5 myThread5 = new MyThread5();
+        myThread5.setName("moy_potok");
+        myThread5.setPriority(10);
+
+        System.out.println("Name of mtThread5 = " + myThread5.getName() +
+                " Priority of myThread5 = " + myThread5.getPriority());
+
+    }
+}
+
+class MyThread5 extends Thread {
+    public void run() {
+        System.out.println("privet");
+    }
+}
---
+++ src/Multithreading/Ex6.java
@@ -0,0 +1,15 @@
+package Multithreading;
+
+public class Ex6 implements Runnable{
+    public void run(){
+        System.out.println("method run. Thread name = " +
+                Thread.currentThread().getName());
+    }
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new Ex6());
+        thread.start();
+        System.out.println("method main. Thread name = " +
+                Thread.currentThread().getName());
+    }
+}
