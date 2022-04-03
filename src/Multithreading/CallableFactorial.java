f0295821c2c4fc591af401dbd42afd8640d987cc
andrejosadcuk <Rudyst.1995@gmail.com>
2022-04-03 17:56:52 +0300

    udemy course update Callable

---
+++ src/Multithreading/CallableFactorial.java
@@ -0,0 +1,48 @@
+package Multithreading;
+
+import java.util.concurrent.*;
+
+public class CallableFactorial {
+    static int factorialResult;
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Factorial2 factorial2 = new Factorial2(5);
+        Future<Integer> future = executorService.submit(factorial2);
+        try {
+            System.out.println(future.isDone());
+            System.out.println("Hotim poluchit rezultat");
+            factorialResult = future.get();
+            System.out.println("Poluchili rezultat");
+            System.out.println(future.isDone());
+        } catch (ExecutionException e) {
+            System.out.println(e.getCause());
+        }
+        finally {
+            executorService.shutdown();
+        }
+        System.out.println(factorialResult);
+    }
+}
+
+class Factorial2 implements Callable<Integer> {
+    int f;
+
+    public Factorial2(int f) {
+        this.f = f;
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        if (f <= 0) {
+            throw new Exception("Vi vveli nevernoe chislo");
+        }
+        int result = 1;
+        for (int i = 1; i <= f; i++) {
+            result *= i;
+            Thread.sleep(1000);
+        }
+        return result;
+
+    }
+}
